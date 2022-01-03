9463bc1577d7a6cd61374bfb560ad46f2d80b819
AudreyNicolle <91120501+AudreyNicolle@users.noreply.github.com>
2022-01-03 17:03:57 +0100

    Update mechant.py

--- mechant.py
+++ mechant.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Dec 21 21:17:22 2021
-
 @author: emma1
 """
-
+import tkinter as tk
+from PIL import Image, ImageTk
 
 
 class mechant :
@@ -18,7 +18,10 @@ class mechant :
         self.__canvas = canvas
         self.__x= x
         self.__y = y
-        self.mechant = canvas.create_rectangle( self.__x+20, self.__y+20, self.__x+40, self.__y+40, fill='red')
+        #self.mechant = canvas.create_rectangle( self.__x+20, self.__y+20, self.__x+40, self.__y+40, fill='red')
+        self.image = Image.open("sorcier.png")
+        self.redi_image = ImageTk.PhotoImage(self.image.resize((50,50))) 
+        self.mechant = self.__canvas.create_image(self.__x,self.__y, image = self.redi_image )
         self.largeure = 20
         self.height = 20
         self.dir = 1
@@ -30,8 +33,8 @@ class mechant :
         
     def move(self): 
         
-        #les conditions pour que l'éney ne sorte pas de l'écran
-        if self.__x >= 900:
+        #les conditions pour que l'énemy ne sorte pas de l'écran
+        if self.__x >= 800:
             self.dir = -1
             #print("gauche")
                 
@@ -40,14 +43,22 @@ class mechant :
             self.dir =1
             #print("droite")
         
-        self.__x += self.vel*self.dir
         #print(self.__x)
         
         
         # on bouge l'enemy
-        self.__canvas.coords(self.mechant, self.__x+20, self.__y+20, self.__x+40, self.__y+40)
+        self.__canvas.move(self.mechant,self.vel*self.dir,0)
+        self.__x = self.__canvas.bbox(self.mechant)[0]
+        print(self.__x)
         #print("déplacement")
         self.__canvas.after(1, self.move)
 
+    def image_perso (self) : 
+        
+        image_me = tk.PhotoImage(file = "yeti.png" )
+        self.__canvas.create_image(0,0,image = image_me )
+        
+        
+        
         
         
